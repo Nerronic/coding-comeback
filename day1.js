957f00365bf1486783573e27e3d34124e8d3cdbb
Ty <tyrellcarlton@gmail.com>
2026-06-16 06:52:51 -0700

    first commit

---
+++ day1.js
@@ -0,0 +1,9 @@
+function SayHello() {
+  return "Hello";
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+const numbers = [1, 2, 3, 4, 5];
---
+++ day2.js
@@ -0,0 +1,15 @@
+function multiply(a, b) {
+    return a*b;
+}
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+function getFullName(first,last) {
+    return first + " " + last
+ }
+
+ function square(number){
+    return number ** 2;
+ }
